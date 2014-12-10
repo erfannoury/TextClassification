e0c168f747bcf7ff7b25fb497aa0d677975bb140
Erfan Noury <erfannoury@users.noreply.github.com>
2014-12-10 03:33:15 +0330

    Updated documentation for util.py

--- util.py
+++ util.py
@@ -14,9 +14,20 @@ pattern = re.compile(r'([a-zA-Z]+|[0-9]+(\.[0-9]+)?)')
 def tokenizeDoc(doc_address, min_len = 0, remove_numerics=True):
     """
     to tokenize a document file to alphabetic tokens use this function.
-    doc_address: path to the file that is going to be tokenized
-    min_len: minimum length of a token. Default value is zero, it should always be non-negative.
-    remove_numerics: whether to remove the numeric tokens or not
+
+    Parameters
+    ----------
+    doc_address: str
+                 path to the file that is going to be tokenized
+    min_len: int
+             minimum length of a token. Default value is zero, it should always be non-negative.
+    remove_numerics: boolean
+                     whether to remove the numeric tokens or not
+
+    Returns
+    -------
+    tokens: list
+            list of tokens from the input document according to the filtering criteria specified
     """
     from string import punctuation, digits
     tokens = []
@@ -41,16 +52,24 @@ def tokenizeDoc(doc_address, min_len = 0, remove_numerics=True):
 def createDictionary(classes, tokens_pool):
     """
     this method will create a dictionary out of the tokens_pool it has been provided.
-    classes: this is a list of the names of the classes documents belong to
-    tokens_pool: a pool (in fact implemented as a dictionary) of tokens. Each value of the dictionary is an list of lists,
+
+    Parameters
+    ----------
+    classes: list
+             list of the names of the classes of documents
+    tokens_pool: dictionary
+                 dictionary of tokens. Each value of the dictionary is an list of lists,
                  each list belonging to a document in the corresponding class that has a list of tokens
 
-    output:
-            *Note that the tokens in the dictionary are not sorted, since in the vector space model
-             that we are going to use, all words are treated equal.
-                 We practically believe in justice. Words in dictionary are tired of
-                 all this injustice they have been forced to take for such a long time.
-                 Now is the time to rise and earn the justice that belongs to them.
+
+    Returns
+    -------
+    token_dict: dictionary
+                *Note that the tokens in the dictionary are not sorted, since in the vector space model
+                that we are going to use, all words are treated equal.
+                We practically believe in justice. Words in dictionary are tired of
+                all this injustice they have been forced to take for such a long time.
+                Now is the time to rise and earn the justice that belongs to them.
     """
 
     token_dict = {}
@@ -75,11 +94,19 @@ def createDictionary(classes, tokens_pool):
 def createTokenPool(classes, paths):
     """
     this method will create a pool of tokens out of the list of paths to documents it will be provided
-    classes: a list of the names of the classes documents belong to
-    paths: a dictionary of lists of paths to documents
 
-    output: a dictionary of lists of lists of tokens. each value bin of dictionary is a has a list of lists,
-            for which each list is of a document and it contains a list of tokens in that document
+    Parameters
+    ----------
+    classes: list
+             list of the names of the classes documents belong to
+    paths: dictionary
+           dictionary of lists of paths to documents
+
+    Returns
+    -------
+    token_pool: dictionary
+                dictionary of lists of lists of tokens. each value bin of dictionary is a has a list of lists,
+                for which each list is of a document and it contains a list of tokens in that document
     """
     token_pool = {}
     for cl in classes:
@@ -94,10 +121,17 @@ def createTokenPool(classes, paths):
 def saveDictToFile(tdict, filename):
     """
     this method will save the key/value pair of the dictionary to a csv file
-    tdict: a dictionary object containing many pairs of key and value
-    filename: name of the dictionary file
 
-    output: a csv file in which dictionary is dumped
+    Parameters
+    ----------
+    tdict: dictionary
+           dictionary object containing many pairs of key and value
+    filename: str
+              name of the dictionary file
+
+
+    Returns
+    -------
     """
     import csv
     w = csv.writer(open(filename, "w"))
@@ -115,10 +149,16 @@ def saveDictToFile(tdict, filename):
 def readFileToDict(filename):
     """
     this method will create a dictionary from a file
-    filename: name of the dictionary file
     *dictionary file is a csv file, each row contains a token and it's index
+    Parameters
+    ----------
+    filename: str
+              name of the dictionary file
 
-    output: a dictionary object created from input file
+    Returns
+    -------
+    tdict: dictionary
+           dictionary object created from input file
     """
     import csv, codecs
     tdict = {}
@@ -137,11 +177,24 @@ def readFileToDict(filename):
 
 def train_test_split(ratio, classes, files):
     """
-    this method will split the input list of files to train and test sets
+    this method will split the input list of files to train and test sets.
+    *Note: currently this method uses the simplest way an array can be split in two parts.
 
-    output: a tuple of train and test files after splitting
+    Parameters
+    ----------
+    ratio: float
+           ratio of total documents in each class assigned to the training set
+    classes: list
+             list of label classes
+    files: dictionary
+           a dictionary with list of files for each class
 
-    *Note: currently this method uses the simplest way an array can be split in two parts
+    Returns
+    -------
+    train_dic: dictionary
+               a dictionary with lists of documents in the training set for each class
+    test_dict: dictionary
+               a dictionary with lists of documents in the testing set for each class
     """
     train_dict = {}
     test_dict = {}
